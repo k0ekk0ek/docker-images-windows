66ba65c63864306f7ff96d0f5927f02a221a2a1f
Jeroen Koekkoek <jeroen@koekkoek.nl>
2020-12-06 16:14:41 +0100

    Test images

--- push_docker.bat
+++ push_docker.bat
@@ -1,16 +1,5 @@
 @echo off
 
-IF not "%CIRRUS_BRANCH%" == "master" GOTO NOTMASTER
-:YESPATH
-
 docker login --username "%DOCKER_USER_NAME%" --password "%DOCKER_PASSWORD%"
-docker push cirrusci/windowsservercore:2019
-docker push cirrusci/windowsservercore:cmake
-
-GOTO END
-:NOTMASTER
-
-@ECHO Not a master build. Nothing to push!
-
-GOTO END
-:END
+docker push k0ekk0ek/windowsservercore:2019
+docker push k0ekk0ek/windowsservercore:cmake
